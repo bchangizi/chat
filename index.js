8dbae881cfb22af394ae899210f34390a580b44f
andreigabur <andrei.gabur@yahoo.com>
2015-03-06 17:43:23 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var usernames = {};
+
+io.on('connection', function(socket){
+
+  socket.on('adduser', function(username){
+		socket.username = username;
+		usernames[username] = username;
+		socket.emit('updateroom', username);
+    socket.emit('updatechat', 'SERVER', username + ' has connected to this chat');
+	});
+
+  socket.on('sendchat', function(data){
+    io.emit('updatechat', socket.username, data);
+  });
+});
+
+http.listen(3000, function () {
+  console.log('listening on *:3000');
+});
