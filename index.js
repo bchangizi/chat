5eff774e832d70042ee5ce3a54ef30862c8a51db
andrei <andrei.gabur@yahoo.com>
2015-03-07 16:16:57 +0200

    jslint __dirname permit and intentation

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-/*jslint node: true */
+/*jslint node: true, nomen: true*/
 "use strict";
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
 var usernames = {};
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
 
-  socket.on('adduser', function(username){
+    socket.on('adduser', function (username) {
 		socket.username = username;
 		usernames[username] = username;
 		socket.emit('updateroom', username);
-    socket.emit('updatechat', 'SERVER', username + ' has connected to this chat');
+        socket.emit('updatechat', 'SERVER', username + ' has connected to this chat');
 	});
 
-  socket.on('sendchat', function(data){
-    io.emit('updatechat', socket.username, data);
-  });
+    socket.on('sendchat', function (data) {
+        io.emit('updatechat', socket.username, data);
+    });
 });
 
 http.listen(3000, function () {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
